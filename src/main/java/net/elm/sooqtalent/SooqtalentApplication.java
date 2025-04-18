3f6cc9274f9f9e2481e34af021f7d2dacfacf3ed
abderrahim <elmoutaouakil12345@gmail.com>
2025-04-18 19:40:29 +0100

    initialize project with Spring Boot and Maven setup

---
+++ src/main/java/net/elm/sooqtalent/SooqtalentApplication.java
@@ -0,0 +1,13 @@
+package net.elm.sooqtalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooqtalentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SooqtalentApplication.class, args);
+    }
+
+}
