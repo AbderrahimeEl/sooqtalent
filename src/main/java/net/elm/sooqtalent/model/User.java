3ec8463390203b6835cc0824ede5ffecdde42954
abderrahim <elmoutaouakil12345@gmail.com>
2025-04-18 20:00:51 +0100

    add User entity

--- src/main/java/net/elm/sooqtalent/model/User.java
+++ src/main/java/net/elm/sooqtalent/model/User.java
@@ -1,4 +1,17 @@
 package net.elm.sooqtalent.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+
 }
