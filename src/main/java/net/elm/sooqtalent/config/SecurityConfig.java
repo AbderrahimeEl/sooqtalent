59e3a74218a12506f3741f091db99a232f3d6159
abderrahim <elmoutaouakil12345@gmail.com>
2025-04-18 23:32:45 +0100

    add JWT authentication filter and utility for token management

--- src/main/java/net/elm/sooqtalent/config/SecurityConfig.java
+++ src/main/java/net/elm/sooqtalent/config/SecurityConfig.java
@@ -1,22 +1,27 @@
 package net.elm.sooqtalent.config;
 
+import net.elm.sooqtalent.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/register").permitAll()
-                        .requestMatchers("/api/users").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers("/api/users").authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
---
+++ src/main/java/net/elm/sooqtalent/security/JwtUtil.java
@@ -0,0 +1,47 @@
+package net.elm.sooqtalent.security;
+
+import io.jsonwebtoken.*;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private String secretKey = "123";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return (username.equals(extractUsername(token)) && !isTokenExpired(token));
+    }
+}
